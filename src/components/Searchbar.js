9853f226bd9dc0ccbdc09a60ba4270dfb070e921
Kevin Poppe <kevin.poppe93@gmail.com>
2022-04-22 14:43:52 +0200

    debugged fetch

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,15 +1,18 @@
-import React, { useState, useEffect } from "react";
-import Map from "./Map";
-import "../styles/searchbar.css";
+import React, { useState, useEffect } from 'react';
+import Map from './Map';
+import '../styles/searchbar.css';
+import axios from 'axios';
 
-export default function Searchbar({changeData, data}) {
-  const [userInput, setUserInput] = useState("");
+export default function Searchbar({ changeData, data }) {
+  const [userInput, setUserInput] = useState('');
 
-useEffect(() => {
-  fetch(`https://crossover-2.herokuapp.com/search/${userInput}`)
-        .then(response => response.json())
-        .then(data => setUserInput(data))
-        .catch(error => console.log('Error: ', error))}, [userInput]);
+  function fetchData() {
+    axios(`http://crossover-2.herokuapp.com/search/${userInput}`)
+      .then((res) => {
+        if (res) changeData(res.data);
+      })
+      .catch((error) => console.log('Error: ', error));
+  }
 
   const handleChange = (e) => {
     setUserInput(e.target.value);
@@ -17,29 +20,28 @@ useEffect(() => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    changeData(fetch(userInput).data);
+    fetchData();
   };
 
   return (
     <>
-      <div className="nav">
-        <div className="search">
-          <div className="searchbar">
+      <div className='nav'>
+        <div className='search'>
+          <div className='searchbar'>
             <input
-              type="text"
-              placeholder="Search for a place..."
+              type='text'
+              placeholder='Search for a place...'
               onChange={(e) => handleChange(e)}
             />
-            <button className="search-button" onClick={handleSubmit}>Search</button>
+            <button className='search-button' onClick={handleSubmit}>
+              Search
+            </button>
           </div>
           {data.map((place) => (
-              <Map 
-              key={place.id}
-              name={place.name}
-              />
-            ))}
+            <Map key={place.id} name={place.name} />
+          ))}
         </div>
-        <div className="header">
+        <div className='header'>
           <h1>CrossoverMap</h1>
         </div>
       </div>
